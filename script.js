d24ce3365fe0e5feb85db9de9f53c3d6522ef771
Yehor <remziq1992@gmail.com>
2021-02-07 23:45:17 +0300

    buoy.

--- script.js
+++ script.js
@@ -3,114 +3,71 @@
 const numberBtns = document.querySelectorAll('.number');
 const operationBtns = document.querySelectorAll('.operation');
 const clearBtns = document.querySelectorAll('.clear-btn');
+const equalButton = document.getElementById('equals');
 const decimalBtn = document.getElementById('decimal');
 const display = document.getElementById('display');
-let memoryCurrentNumber = 0;				// Текущее число
-let memoryNewNumber = false;				// Вводим новое число или продолжаем вводить старое
-let memoryPendingOperation = '';			// Операция, ожидающая подтверждения
 
+let firstOperand = '';
+let secondOperand = '';
+let operation = undefined;
+let result = undefined;
 
-
-// Вешаем события на кнопки
-
-for (let i = 0; i < numberBtns.length; i++) {						// Кнопки с числами
-	let numberBtn = numberBtns[i];
-
-	numberBtn.addEventListener('click', function (e) {
-		numberPress(e.target.textContent);
-	});
-}
-
-
-for (let i = 0; i < operationBtns.length; i++) {					// Кнопки с операциями
-	let operationBtn = operationBtns[i];
-
-	operationBtn.addEventListener('click', function (e) {
-		operation(e.target.textContent);
+const addListners = (nodesList, callback) => {
+	nodesList.forEach(node => {
+		node.addEventListener('click', e => {
+			callback(e);
+		})
 	})
-}
+};
 
-
-for (let i = 0; i < clearBtns.length; i++) {							// Кнопки очистки
-	let clearBtn = clearBtns[i];
-
-	clearBtn.addEventListener('click', function (e) {
-		clearDisplay(e.srcElement.id);
-	})
-}
-
-
-decimalBtn.addEventListener('click', addDecimal);					// Кнопка десятичной дроби
-
-
-
-// Что происходит при нажатии на отдельные кнопки
-
-function numberPress(number) {
-
-	if (memoryNewNumber) {
-		display.value = number;
-		memoryNewNumber = false;
+const numberPressHandler = (e) => {
+	if(operation) {
+		secondOperand += e.target.textContent;
+		display.value = secondOperand;
 	} else {
-
-		if (display.value === '0') {
-			display.value = number;
-		} else {
-			display.value += number;
-		}
+		firstOperand += e.target.textContent;
+		display.value = firstOperand;
 	}
+};
 
-}
+const calculateResult = () => {
+	switch (operation) {
+		case '+':
+		  result = +firstOperand + +secondOperand;
+		  break;
+		case '-':
+			result = +firstOperand - +secondOperand;
+		  break;
+		case '*':
+			result = +firstOperand * +secondOperand;
+		  break;
+		case '/':
+			result = +firstOperand / +secondOperand;
+		break;
+		default:
+		  throw new Error('unexpected operation! bitch');
+	  }
+};
 
-
-function operation(symbol) {
-
-	let localOperationMemory = display.value;
-
-	if (memoryNewNumber && memoryPendingOperation !== '=') {
-		display.value = memoryCurrentNumber;
-	} else {
-		memoryNewNumber = true;
-
-		if (memoryPendingOperation === '+') {
-			memoryCurrentNumber += +localOperationMemory;
-		} else if (memoryPendingOperation === '-') {
-			memoryCurrentNumber -= +localOperationMemory;
-		} else if (memoryPendingOperation === '*') {
-			memoryCurrentNumber *= +localOperationMemory;
-		} else if (memoryPendingOperation === '/') {
-			memoryCurrentNumber /= +localOperationMemory;
-		} else {
-			memoryCurrentNumber = +localOperationMemory;
-		}
+const operationHandler = (e) => {
+	const operationSymbol = e.target.textContent;
+	if(operation) {
+		calculateResult();
+		firstOperand = result;
+		secondOperand = '';
 	}
+	operation = operationSymbol;
+	display.value = operationSymbol;
+};
 
-	display.value = memoryCurrentNumber;
-	memoryPendingOperation = symbol;
-}
+const equalHandler = () => {
+	calculateResult();
+	display.value = result;
+};
 
+addListners(numberBtns, numberPressHandler);
+addListners(operationBtns, operationHandler);
 
-function clearDisplay(id) {
-
-	display.value = '0';
-
-	if (id === 'ce') {
-		memoryNewNumber = true;
-	} else {
-		memoryCurrentNumber = 0;
-		memoryNewNumber = false;
-		memoryPendingOperation = '';
-	}
-}
-
-
-function addDecimal() {
-
-	let localDecimalMemory = display.value;
-
-	if (localDecimalMemory.includes('.') === false) {
-		localDecimalMemory += '.';
-	}
-
-	display.value = localDecimalMemory;
-}
+equalButton.addEventListener('click', e => {
+	equalHandler();
+});
