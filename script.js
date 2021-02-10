fe35009f79de4f30e5afa16d07f4ec87200c2714
marksmirnow <mark.smirnow2@gmail.com>
2021-02-11 00:08:13 +0300

    feat: get elements of calculator

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const display = document.getElementById('display');
+const numberBtns = document.querySelectorAll('.number');
+const operationBtns = document.querySelectorAll('.operation');
+const equalBtn = document.getElementById('equals');
+const clearBtns = document.querySelectorAll('.clear-btn');
+const decimalBtn = document.getElementById('decimal');
