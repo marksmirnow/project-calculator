3bae838f4a910f3ba5a897a40b560a4ddd24a299
marksmirnow <mark.smirnow2@gmail.com>
2021-02-14 01:50:00 +0300

    feat: add function addListener

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const operationBtns = document.querySelectorAll('.operation');
 const equalBtn = document.getElementById('equals');
 const clearBtns = document.querySelectorAll('.clear-btn');
 const decimalBtn = document.getElementById('decimal');
+
+
+function addListener(nodeList, callback) {
+
+	nodeList.forEach(nodeElem => {
+		nodeElem.addEventListener('click', (event) => {
+			callback(event);
+		})
+	})
+}
